0caa3c36e58c9ca4e81151a4baa53b1771b4c02d
rowenasclai <rowena.sc.lai@gmail.com>
2026-03-02 13:04:32 +0800

    add result

---
+++ PY/exhibit_L1.py
@@ -0,0 +1,112 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+import os
+
+import asyncio
+import json
+
+#page.locator("div").filter(has_text=re.compile(r"^Shown 11-20 of Total Result 1820$"))
+
+#url='https://www.hktdc.com/event/hkjewellery/en/exhibitor-list?pageNum=1&pageSize=50'
+domain='https://www.hktdc.com'
+
+prefix=input('What is the prefix of your exhibition?')
+os.chdir("/Users/rowena/Other Projects/external_data_study/Result/HKTDC/") 
+os.makedirs(prefix, exist_ok=True)
+os.chdir("/Users/rowena/Other Projects/external_data_study/Result/HKTDC/"+prefix) 
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    #l=page.locator("div").filter(has_text=re.compile(r"^Total Result$"))
+    #print(l.inner_text())
+    url='https://www.hktdc.com/event/'+prefix+'/en/exhibitor-list?pageNum=1&pageSize=50'
+    page.goto(url)
+    check_figure=page.locator("div").filter(has_text=re.compile(r"^Shown.*Total Result"))
+    final_figure_text=check_figure.text_content()
+    result_l=final_figure_text.find('Total Result')
+    l_div=int(final_figure_text[result_l+13:])
+
+    print('Processing '+str(1)+' - '+str(50)+' out of '+str(l_div))
+
+    for j in range(1,int(l_div/50)+2,1):
+        url='https://www.hktdc.com/event/'+prefix+'/en/exhibitor-list?pageNum='+str(j)+'&pageSize=50'
+        print('Processing '+str((j-1)*50+1)+' - '+str((j-1)*50+49)+' out of '+str(l_div))
+        page.goto(url)
+        data={}
+    #test=page.locator(".d-flex.flex-column.vep-p-4")
+    #check=page.locator(".d-flex.flex-column.vep-p-4.div").filter(has_text=True).first
+    #link=check.get_by_role("link", name=check).get_attribute("href")
+    #check1=page.locator("div").filter(has_text=True).nth(1)
+    #check1=page.locator("div:nth-child(1) > .d-flex.flex-column.vep-p-4")
+        try:
+            check1=page.locator(".d-flex.flex-column.vep-p-4").first
+            my_list=check1.inner_text().split('\n')
+            data['Company Name']=check1.inner_text().split('\n')[0]
+            data['Location']=check1.inner_text().split('\n')[1]
+            pattern = r'^Booth.*'  # Matches items ending with "berry"
+            # Create a new list containing only the items that match the pattern
+            matching_items = [item for item in my_list if re.search(pattern, item)]
+            data['Booth']=matching_items
+            data['url']=check1.get_by_role('link', name= check1.inner_text().split('\n')[0]).get_attribute("href")
+
+            with open('hktdc_'+prefix+'_L1.json', "a") as f:
+                json_record = json.dumps(data)
+                f.write(json_record + '\n')
+        except:
+            pass
+    #print(check1.inner_text())
+    #print(data)
+
+        for i in range(2,51,1):  
+            if (j-1)*50+i-1<l_div:
+                #print('Processing '+str((j-1)*50+1)+' - '+str((j-1)*50+49)+' out of '+str(l_div))
+                check=page.locator("div:nth-child("+str(i)+") > .d-flex.flex-column.vep-p-4")
+        #print(check.inner_text())
+                data['Company Name']=check.inner_text().split('\n')[0]
+                data['Location']=check.inner_text().split('\n')[1]
+                pattern = r'^Booth.*'  # Matches items ending with "berry"
+            # Create a new list containing only the items that match the pattern
+                matching_items = [item for item in my_list if re.search(pattern, item)]
+                data['Booth']=matching_items
+                
+                try:
+                    data['url']=check.get_by_role('link', name= data['Company Name']).get_attribute("href")
+                except:
+                    pass
+                
+                with open('hktdc_'+prefix+'_L1.json', "a") as f:
+                    json_record = json.dumps(data)
+                    f.write(json_record + '\n')
+            else:
+                pass
+        #print(data)
+    
+    #print(check1.inner_text())
+    #print(link)
+
+    
+    #page.locator("div:nth-child(2) > .d-flex.flex-column.vep-p-4")
+    #all_links=test.get_by_role('link').get_attribute("href")
+
+    # card_box=page.get_by_text("Company DetailsNature of")
+    # all_text=card_box.inner_text()
+    # data[all_text.split('\n')[1]]=all_text.split('\n')[2]
+    #card_box=page.locator(".d-flex.flex-column.col-12")
+
+    #print(all_links)
+
+    #print(data)
+
+    #print(name_el.inner_text().split('\n')[3])
+    #data['company_name'] = name_el
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
+
---
+++ PY/exhibit_L2.py
@@ -0,0 +1,177 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+import csv
+
+#import asyncio
+import json
+
+import os
+#from playwright.async_api import async_playwright
+
+#from concurrent.futures.thread import ThreadPoolExecutor
+
+import pandas as pd
+
+prefix=input('What is the prefix of your exhibition?')
+
+os.chdir("/Users/rowena/Other Projects/external_data_study/Result/HKTDC/"+prefix) 
+df2_L1=pd.read_csv('hktdc_'+prefix+'_L1.csv')
+#df2_L1=pd.read_csv('hktdc_hkdgp_L1.csv')
+domain='https://www.hktdc.com'
+
+def scrape(page):
+    df={}
+
+    try:
+        card_box=page.locator(".d-flex.flex-column.col-12")
+        all_text=card_box.inner_text().split('\n')
+
+    # Locate all direct child div elements using the child combinator (>)
+    # The selector will be '#parent_div > div'
+        child_div_locator = card_box.locator("> div")
+
+    # Use the count() method to get the number of matching elements
+        count = child_div_locator.count()
+
+    #print(count)
+
+        card_box1=page.locator(".d-flex.flex-column.col-12 > div:nth-child(1)")
+        content=card_box1.inner_text().split('\n')
+        df['company name']= ' '.join(content[0:])
+
+        card_box1=page.locator(".d-flex.flex-column.col-12 > div:nth-child(2)")
+        content=card_box1.inner_text().split('\n')
+        df['location']= ' '.join(content[0:])
+
+        card_box1=page.locator(".d-flex.flex-column.col-12 > div:nth-child(3)")
+        content=card_box1.inner_text().split('\n')
+        if 'Booth:' in content==True:
+            df['Booth']= ' '.join(content[0:])
+
+        for i in range(4,count+1,1):
+            card_box1=page.locator(".d-flex.flex-column.col-12 > div:nth-child("+str(i)+")")
+            content=card_box1.inner_text().split('\n')
+            if 'Booth:' in content[0]==True:
+                df['Booth']=content[0]
+            else:
+                df[content[0]]=' '.join(content[1:])
+    
+       
+        if card_box.get_by_role('link', name= 'View more about this company').count()>0:
+            supplier_url=card_box.get_by_role('link', name= 'View more about this company').get_attribute("href")
+            df['supplier_url']=supplier_url
+        else:
+            df['supplier_url']='N/A'
+
+        df['exhibitor_url']=page.url
+    
+    except:
+        df={}
+
+    with open('hktdc_'+prefix+'_L2.json', "a") as f:
+        json_record = json.dumps(df)
+        f.write(json_record + '\n')
+        
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+
+    j=0
+    page = context.new_page()
+    page.goto(domain+df2_L1['url'][0])
+    scrape(page)
+
+    page1 = context.new_page()
+    page2 = context.new_page()
+    page3 = context.new_page()
+    page4 = context.new_page()
+    page5 = context.new_page()
+    page6 = context.new_page()
+    page7 = context.new_page()
+    page8 = context.new_page()
+    page9 = context.new_page()
+    page10 = context.new_page()
+    page11 = context.new_page()
+    page12 = context.new_page()
+    page13 = context.new_page()
+    page14 = context.new_page()
+    page15 = context.new_page()
+    page16 = context.new_page()
+    page17 = context.new_page()
+    page18 = context.new_page()
+    page19 = context.new_page()
+    page20 = context.new_page()
+
+
+
+    l=int(len(df2_L1)/20)
+    #for j in range(55,l+1,1):
+    
+    for j in range(0,l,1):
+        print('Processing '+str(20*j+1)+' to '+str(20*j+20+1)+' out of '+str(len(df2_L1)))
+        page1.goto(domain+df2_L1['url'][20*j+1], wait_until="domcontentloaded")
+        page2.goto(domain+df2_L1['url'][20*j+2], wait_until="domcontentloaded")
+        page3.goto(domain+df2_L1['url'][20*j+3], wait_until="domcontentloaded")
+        page4.goto(domain+df2_L1['url'][20*j+4], wait_until="domcontentloaded")
+        page5.goto(domain+df2_L1['url'][20*j+5], wait_until="domcontentloaded")
+        page6.goto(domain+df2_L1['url'][20*j+6], wait_until="domcontentloaded")
+        page7.goto(domain+df2_L1['url'][20*j+7], wait_until="domcontentloaded")
+        page8.goto(domain+df2_L1['url'][20*j+8], wait_until="domcontentloaded")
+        page9.goto(domain+df2_L1['url'][20*j+9], wait_until="domcontentloaded")
+        page10.goto(domain+df2_L1['url'][20*j+10], wait_until="domcontentloaded")
+        page11.goto(domain+df2_L1['url'][20*j+11], wait_until="domcontentloaded")
+        page12.goto(domain+df2_L1['url'][20*j+12], wait_until="domcontentloaded")
+        page13.goto(domain+df2_L1['url'][20*j+13], wait_until="domcontentloaded")
+        page14.goto(domain+df2_L1['url'][20*j+14], wait_until="domcontentloaded")
+        page15.goto(domain+df2_L1['url'][20*j+15], wait_until="domcontentloaded")
+        page16.goto(domain+df2_L1['url'][20*j+16], wait_until="domcontentloaded")
+        page17.goto(domain+df2_L1['url'][20*j+17], wait_until="domcontentloaded")
+        page18.goto(domain+df2_L1['url'][20*j+18], wait_until="domcontentloaded")
+        page19.goto(domain+df2_L1['url'][20*j+19], wait_until="domcontentloaded")
+        page20.goto(domain+df2_L1['url'][20*j+20], wait_until="domcontentloaded")
+
+        scrape(page1)
+        scrape(page2)
+        scrape(page3)
+        scrape(page4)
+        scrape(page5)
+        scrape(page6)
+        scrape(page7)
+        scrape(page8)
+        scrape(page9)
+        scrape(page10)
+        scrape(page11)
+        scrape(page12)
+        scrape(page13)
+        scrape(page14)
+        scrape(page15)
+        scrape(page16)
+        scrape(page17)
+        scrape(page18)
+        scrape(page19)
+        scrape(page20)
+
+    r=len(df2_L1) %20
+    
+    if r==0:
+        f=20
+        l=int(len(df2_L1)/20)-1
+    else:
+        f=r
+        l=int(len(df2_L1)/20)
+
+    print('Processing '+str(20*l+1)+' to '+str(20*l+f)+' out of '+str(len(df2_L1)))
+    for i in range(1,f,1):
+        page1.goto(domain+df2_L1['url'][20*l+i])
+        scrape(page1)
+        #print(20*l+i)
+        
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
+
