15a2143c24ea444f2146af6458c3c330cb69d5fe
rowenasclai <rowena.sc.lai@gmail.com>
2026-03-03 09:41:02 +0800

    add in messe Frankfurt result

---
+++ PY/messe_frankfurt.py
@@ -0,0 +1,56 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+import csv
+
+import asyncio
+import json
+
+
+url="https://asiamold-china.cn.messefrankfurt.com/guangzhou/en/exhibitor-search.html?page=2&pagesize=90"
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page1 = context.new_page()
+    page1.goto(url)
+
+    data={}
+
+    for i in range(90):
+
+        exhibitor_list=page1.locator(".ex-exhibitor-search-results-container")
+
+        item=exhibitor_list.locator("div.m-search-result-item.ex-exhibitor-search-result-item.ex-exhibitor-search-result-item__grid.ex-exhibitor-search-result-item__inner").nth(i)
+
+        data['company_name']=item.locator("h4.ex-exhibitor-search-result-item__headline").inner_text()
+
+        data['description']=item.locator("p.ex-exhibitor-search-result-item__copy").inner_text()
+        data['exhibit loc']=item.locator("div.ex-exhibitor-search-result-item__location").inner_text()
+
+    #item_url_loc=exhibitor_list.locator("div.ex-exhibitor-search-results-container.ex-exhibitor-search-results-container--grid-cols.show-confair-container")
+
+    #page.get_by_role("link", name="ARTOP MOLD INDUSTRIAL CO.,").click()
+    #page.locator(".slick-slider.m-tab-slider__slider > .slick-list > .slick-track > .slick-slide.slick-active").click(button="right")
+
+
+        data['url']=page1.get_by_role("link", name=data['company_name']).get_attribute("href")
+
+        with open('asia_mold.json', "a") as f:
+            json_record = json.dumps(data)
+            f.write(json_record + '\n')  
+
+    
+    #print(item.inner_text())
+    #print(item_co.inner_text())
+    #print(item_desc.inner_text())
+    #print(item_loc.inner_text())
+    #print(item_url)
+
+
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
